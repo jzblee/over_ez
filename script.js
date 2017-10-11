db5fe888b71f8e7c529b1db3d270e1a23c89176f
Jason Lee <jason@jzblee.com>
2017-10-11 18:40:24 -0400

    Add base files for the complete digest generator
    Includes empty sections corresponding to final digest
    Also incorporates styling for section and output elements

---
+++ script.js
@@ -0,0 +1,33 @@
+init();
+
+function init() {
+  var fieldsets = document.getElementsByTagName('fieldset');
+  for (var i = fieldsets.length - 1; i >= 0; i--) {
+    fieldsets[i].onmouseover = function() {
+      this.getElementsByTagName('legend')[0].style.borderColor = 'threedhighlight';
+      this.style.borderColor = 'threedhighlight'
+    };
+    fieldsets[i].onmouseout = function() {
+      this.getElementsByTagName('legend')[0].style.borderColor = 'threedface';
+      this.style.borderColor = 'threedface'
+    };
+  }
+  var output = document.getElementById('output');
+  output.onmouseover = function() {
+    this.style.borderColor = 'threedhighlight'
+  };
+  output.onmouseout = function() {
+    this.style.borderColor = 'threedface'
+  };
+  var digestDate = document.getElementById('digestDate');
+  digestDate.value = new Date(Date.now()).toISOString().slice(0, 10);
+}
+
+function nextSunday() {
+  var digestDate = document.getElementById('digestDate');
+  var d = new Date(digestDate.value);
+  while (d.getUTCDay() != 0) {
+    d.setDate(d.getDate() + 1);
+  }
+  digestDate.value = d.toISOString().slice(0, 10);
+}
