9ffbf782ca8be3ff01dfcff9f2a1711e0ac12449
Jason Lee <jason@jzblee.com>
2017-10-13 22:17:48 -0400

    Add boilerplate generation with custom publication date and stylesheet

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 init();
 
 function init() {
@@ -20,24 +23,106 @@ function init() {
     this.style.borderColor = 'threedface'
   };
   today();
+  var formInputs = document.getElementsByTagName('form')[0].getElementsByTagName('input');
+  for (var i = formInputs.length - 1; i >= 0; i--) {
+    formInputs[i].onmouseover = function() {
+      this.style.backgroundColor = '#eee'
+    };
+    formInputs[i].onmouseout = function() {
+      this.style.backgroundColor = '#ddd'
+    };
+  }
 }
 
 function today() {
   var digestDate = document.getElementById('digestDate');
-  digestDate.value = new Date(Date.now()).toISOString().slice(0, 10);
+  var todayDate = new Date(Date.now());
+  digestDate.value = constructDateString(todayDate, "-", false);
 }
 
 function nextSunday() {
   var digestDate = document.getElementById('digestDate');
   if(!digestDate.value) today();
   var d = new Date(digestDate.value);
-  while (d.getUTCDay() != 0) {
+  while (d.getDay() != 0) {
     d.setDate(d.getDate() + 1);
   }
-  digestDate.value = d.toISOString().slice(0, 10);
+  digestDate.value = constructDateString(d, "-", false);
 }
 
 function render() {
+  document.getElementById('genButton').innerHTML = 'REGEN ADVISED';
+  document.getElementById('genButton').style.color = '#f00';
+  document.getElementById('output').style.backgroundColor = '#fdd';
+  document.getElementById('outputText').style.backgroundColor = '#fdd';
   var renderWindow = window.open();
   renderWindow.document.write(document.getElementById('outputText').value);
+}
+
+function gen() {
+  document.getElementById('genButton').innerHTML = 'GENERATE';
+  document.getElementById('genButton').style.color = '#000';
+  document.getElementById('renderButton').disabled = false;
+  document.getElementById('output').style.backgroundColor = '#fff';
+  document.getElementById('outputText').style.backgroundColor = '#fff';
+  var output = "";
+  output += genHeader();
+  output += genFooter();
+  document.getElementById('outputText').value = output;
+}
+
+function genHeader() {
+  var digestDate = new Date(document.getElementById('digestDate').value);
+  var dateString = constructDateString(digestDate, ".", true);
+  var digestCSS = document.getElementById("digestCSS").value;
+  var mdyString = constructMDYString(digestDate, true);
+  var headerStr =
+      '<!doctype HTML>\n'
+    + '<html lang="en">\n'
+    + '  <head>\n'
+    + '    <title>EZ Digest - ' + dateString + '</title>\n'
+    + '    <link rel="stylesheet" type="text/css" href="' + digestCSS + '">\n'
+    + '  </head>\n'
+    + '  <body>\n'
+    + '    <table id="wrapper">\n'
+    + '      <tr>\n'
+    + '        <td id="header">\n'
+    + '          EZ Digest - ' + mdyString + '\n'
+    + '        </td>\n'
+    + '      </tr>\n';
+  return headerStr;
+}
+
+function genFooter() {
+  var footerStr =
+      '      <tr>\n'
+    + '        <td id="footer">\n'
+    + '          <p><em>The goal of the weekly EZ Digest is to provide a concise summary of recent and upcoming chapter activities.</em></p>\n'
+    + '          <p>Feel free to reply to this message with any comments or suggestions!</p>\n'
+    + '          <p>Alpha Phi Omega - Epsilon Zeta Chapter - Rensselaer Polytechnic Institute</p>\n'
+    + '        </td>\n'
+    + '      </tr>\n'
+    + '    </table>\n'
+    + '  </body>\n'
+    + '</html>\n'
+  return footerStr;
+}
+
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+function constructDateString(date, separator, utc) {
+  return utc
+    ? date.getUTCFullYear() + separator + pad(date.getUTCMonth() + 1, 2) + separator + pad(date.getUTCDate(), 2)
+    : date.getFullYear() + separator + pad(date.getMonth() + 1, 2) + separator + pad(date.getDate(), 2);
+}
+
+function constructMDYString(date, utc) {
+  return utc
+    ? months[date.getUTCMonth()] + " " + date.getUTCDate() + ", " + date.getUTCFullYear()
+    : months[date.getMonth()] + " "  + date.getDate() + ", " + date.getFullYear();
+
 }
